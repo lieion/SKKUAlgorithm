ecde9e5f7286b9e71dc811918917f40bd08a2188
JunHyeongKim73 <56298375+JunHyeongKim73@users.noreply.github.com>
2019-12-04 21:57:06 +0900

    Update main.cpp
    City 위치 랜덤 생성, 랜덤 Path 100개 생성 ==> Graph 생성 완료.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,38 @@
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
+#include <cstdio>
 #include <cstring>
+#include <cstdlib>
 #include <time.h>
-//
-typedef struct city {
+
+class City {
+public:
 	char name;
 	int pos_x;
 	int pos_y;
-} City;
+};
 
-typedef struct node {
-	struct node* next;
-	int data;
-}NODE;
+class Node {
+public:	
+	Node* next;
+	City city;
+};
+
+// Adjacency list 그래프 구현에 필요
+class ADJList {
+public:
+	Node* head;
+
+	ADJList() {
+		head = NULL;
+	}
+
+	void insertNode(City city) {
+		Node* temp = new Node();
+		temp->city = city;
+		temp->next = head;
+		head = temp;
+	}
+};
 
 int pathCheck(char random_path[100][3], int num) {
 	for (int i = 0; i < num; i++) {
@@ -23,11 +42,19 @@ int pathCheck(char random_path[100][3], int num) {
 	}
 	return 0;
 }
-int main(void){
-  srand(time(NULL));
-	NODE* head[26];
+
+int main() {
+	srand(time(NULL));
+	ADJList* adjlist = new ADJList[26];
 	char random_path[100][3];
 	
+	City city[26];
+	for (int i = 0; i < 26; i++) {
+		city[i].name = i + 'a';
+		city[i].pos_x = rand() % 6000 - 3000;
+		city[i].pos_y = rand() % 6000 - 3000;
+	}
+
 	for (int i = 0; i < 100; i++) {
 		random_path[i][0] = (rand() % 26) + 'a';
 		random_path[i][1] = (rand() % 26) + 'a';
@@ -36,5 +63,12 @@ int main(void){
 			continue;
 		}
 	}
-  return 0;
+
+	for (int i = 0; i < 100; i++) {
+		char start = random_path[i][0];
+		char dest = random_path[i][1];
+
+		adjlist[i].insertNode(city[start - 'a']);
+		adjlist[i].insertNode(city[dest - 'a']);
+	}
 }
