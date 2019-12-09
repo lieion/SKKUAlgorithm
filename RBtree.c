8efbe51da9c5d3fd296acc264c599c10d4aade64
Jennydream <36976961+Jennydream@users.noreply.github.com>
2019-12-09 23:46:26 +0900

    Update RBtree.c

--- RBtree.c
+++ RBtree.c
@@ -438,6 +438,24 @@ int RBtree_height(struct RBtree_node_t* ptr) {
 	else {
 		int left_h = RBtree_height(ptr->left);
 		int right_h = RBtree_height(ptr->right);
-		return 1 + (left_h > right_h ? left_h : right_h); // µÑ Áß Å« °ª¿¡ 1À» ´õÇØ ¹İÈ¯
+		return 1 + (left_h > right_h ? left_h : right_h); // ÂµÃ‘ ÃÃŸ Ã…Â« Â°ÂªÂ¿Â¡ 1Ã€Â» Â´ÃµÃ‡Ã˜ Â¹ÃÃˆÂ¯
 	}
+}
+int RBtree_height(struct RBtree_node_t* ptr) {
+	if (!ptr)
+		return 0;
+	else {
+		int left_h = RBtree_height(ptr->left);
+		int right_h = RBtree_height(ptr->right);
+		return 1 + (left_h > right_h ? left_h : right_h); 
+	}
+}
+
+
+int count_node(struct RBtree_node_t* ptr) {
+	int count_node_number = 0;
+	
+	if (ptr == NULL) return 0;
+	 count_node_number = 1+ count_node(ptr->left)+ count_node(ptr->right);
+	 return count_node_number;
 }
