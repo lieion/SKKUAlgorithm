c7d55f602c960a6c92bd76371a06752bece2758c
Jennydream <36976961+Jennydream@users.noreply.github.com>
2019-12-10 00:04:23 +0900

    Update RBtree.c

--- RBtree.c
+++ RBtree.c
@@ -432,15 +432,7 @@ int RBtree_search_node(struct RBtree_node_t* tree, int value)
 }
 
 
-int RBtree_height(struct RBtree_node_t* ptr) {
-	if (!ptr)
-		return 0;
-	else {
-		int left_h = RBtree_height(ptr->left);
-		int right_h = RBtree_height(ptr->right);
-		return 1 + (left_h > right_h ? left_h : right_h); // µÑ Áß Å« °ª¿¡ 1À» ´õÇØ ¹ÝÈ¯
-	}
-}
+
 int RBtree_height(struct RBtree_node_t* ptr) {
 	if (!ptr)
 		return 0;
